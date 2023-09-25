d3c55fc562ca03eaa72f3af380547a3b4e39cdd2
gon_g <gon_g@DESKTOP-G76AQNU>
2023-09-25 21:54:03 +0300

    Добавьте файлы проекта.

---
+++ MusicPlayer/Class1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows.Controls;
+using TagLib;
+
+namespace MusicPlayer
+{
+    public class Track
+    {
+        public string Name { get; }
+        public string Path { get; }
+        public Image Image { get; }
+
+        public Track(string path)
+        {
+            Path = path;
+            Name = System.IO.Path.GetFileNameWithoutExtension(path);
+            Image = GetImage();
+        }
+
+        private Image GetImage()
+        {
+            try
+            {
+                var file = TagLib.File.Create(Path);
+                var im = new Image
+                {
+                    Height = 30,
+                    Width = 30,
+                    Stretch = Stretch.Fill
+                };
+
+                if (file.Tag.Pictures.Length > 0)
+                {
+                    var pic = file.Tag.Pictures[0];
+                    var ms = new MemoryStream(pic.Data.Data);
+                    ms.Seek(0, SeekOrigin.Begin);
+
+                    var bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.StreamSource = ms;
+                    bitmap.EndInit();
+                    im.Source = bitmap;
+                }
+                else
+                {
+                    im.Source = new BitmapImage(new Uri($"pack://application:,,,/csharpicons/musicdefault.png"));
+                }
+
+                return im;
+            }
+            catch
+            {
+                var im = new Image
+                {
+                    Source = new BitmapImage(new Uri($"pack://application:,,,/csharpicons/musicdefault.png")),
+                    Height = 30,
+                    Width = 30,
+                    Stretch = Stretch.Fill
+                };
+                return im;
+            }
+        }
+    }
+
+    public class Playlist
+    {
+        public bool IsPaused { get; set; } = false;
+        public bool IsRandom { get; set; } = false;
+        public bool IsRepeat { get; set; } = false;
+        public List<Track> Tracks { get; } = new List<Track>();
+
+        public void LoadTracks(List<string> tracks = null)
+        {
+            if (tracks != null)
+            {
+                foreach (var track in tracks)
+                {
+                    Tracks.Add(new Track(track));
+                }
+            }
+        }
+    }
+
+}
---
+++ MusicPlayer/MainWindow.xaml.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using static System.Net.WebRequestMethods;
+
+namespace MusicPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Playlist playlist = new Playlist();
+        private bool stopUpdating = false;
+        private string folder = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void randomButton_Click(object sender, EventArgs e)
+        {
+            ToggleRandomMode();
+        }
+
+        private void ToggleRandomMode()
+        {
+            playlist.IsRandom = !playlist.IsRandom;
+            randomButtonImage.Source = new BitmapImage(new Uri($"pack://application:,,,/csharpicons/random_{playlist.IsRandom}.png"));
+
+            UpdatePlaylist(playlist.IsRandom);
+            if (MusicList.Items.Count > 0)
+            {
+                MusicList.SelectedIndex = 0;
+            }
+        }
+
+        private void prevButton_Click(object sender, EventArgs e)
+        {
+            if (MusicList.Items.Count == 0)
+            {
+                return;
+            }
+            if (MusicList.SelectedIndex == 0)
+            {
+                MusicList.SelectedIndex = MusicList.Items.Count - 1;
+            }
+            else
+            {
+                MusicList.SelectedIndex -= 1;
+            }
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            TogglePlayback();
+        }
+
+        private void TogglePlayback()
+        {
+            if (playlist.IsPaused)
+            {
+                media.Play();
+            }
+            else
+            {
+                media.Pause();
+            }
+            playlist.IsPaused = !playlist.IsPaused;
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            if (MusicList.Items.Count == 0)
+            {
+                return;
+            }
+            if (MusicList.SelectedIndex == MusicList.Items.Count - 1)
+            {
+                MusicList.SelectedIndex = 0;
+            }
+            else
+            {
+                MusicList.SelectedIndex += 1;
+            }
+        }
+
+        private void repeatButton_Click(object sender, EventArgs e)
+        {
+            ToggleRepeatMode();
+        }
+
+        private void ToggleRepeatMode()
+        {
+            playlist.IsRandom = !playlist.IsRandom;
+            repeatButtonImage.Source = new BitmapImage(new Uri($"pack://application:,,,/csharpicons/repeat_{playlist.IsRepeat}.png"));
+        }
+
+        private void media_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            trackSlider.Maximum = media.NaturalDuration.TimeSpan.Ticks;
+            musicImage.Source = playlist.Tracks[MusicList.SelectedIndex].Image.Source;
+
+            Thread gg = new Thread(delegate () { UpdateSlider(); });
+            stopUpdating = false;
+            gg.Start();
+        }
+
+        private void UpdateSlider()
+        {
+            while (true)
+            {
+                try
+                {
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        int playerIndex = MusicList.SelectedIndex;
+                        if (playerIndex != MusicList.SelectedIndex)
+                        {
+                            return;
+                        }
+                        if (media.NaturalDuration.HasTimeSpan && media.Position.Ticks == media.NaturalDuration.TimeSpan.Ticks)
+                        {
+                            if (!playlist.IsRepeat)
+                            {
+                                if (MusicList.SelectedIndex == MusicList.Items.Count - 1)
+                                {
+                                    MusicList.SelectedIndex = 0;
+                                }
+                                else
+                                {
+                                    MusicList.SelectedIndex += 1;
+                                }
+                            }
+                            else
+                            {
+                                playMusic();
+                            }
+                        }
+                        trackPosition.Text = media.Position.ToString();
+                        try
+                        {
+                            trackMaximum.Text = media.NaturalDuration.TimeSpan.ToString();
+                        }
+                        catch
+                        {
+                            trackMaximum.Text = new TimeSpan().ToString();
+                        }
+                        trackSlider.Value = media.Position.Ticks;
+                    });
+                }
+                catch (TaskCanceledException e)
+                {
+                    Environment.Exit(0);
+                }
+                catch
+                {
+                    return;
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        private void playMusic()
+        {
+            media.Source = new Uri(playlist.Tracks[MusicList.SelectedIndex].Path);
+            media.Play();
+            media.Volume = 1;
+        }
+
+        private void MusicList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (MusicList.SelectedIndex == -1) return;
+            playMusic();
+        }
+
+        private void trackSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            media.Position = new TimeSpan(Convert.ToInt64(trackSlider.Value));
+        }
+
+        private void UpdatePlaylist(bool randomize = false)
+        {
+            MusicList.Items.Clear();
+            playlist.Tracks.Clear();
+            List<string> files = new List<string>();
+            foreach (string fileName in Directory.GetFiles(folder))
+            {
+                if (fileName.EndsWith("mp3"))
+                {
+                    files.Add(fileName);
+                }
+            }
+            if (randomize)
+            {
+                Random rnd = new Random();
+                files = files.OrderBy(x => rnd.Next()).ToList();
+            }
+            playlist.LoadTracks(files);
+            foreach (Track track in playlist.Tracks)
+            {
+                MusicList.Items.Add(track.Name);
+            }
+            media.Stop();
+        }
+
+        private void UploadMusic(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dlg = new CommonOpenFileDialog { IsFolderPicker = true };
+            CommonFileDialogResult opened = dlg.ShowDialog();
+            if (opened != CommonFileDialogResult.Ok)
+            {
+                return;
+            }
+            List<string> files = new List<string>();
+            folder = dlg.FileName;
+            UpdatePlaylist();
+            if (MusicList.Items.Count > 0)
+            {
+                MusicList.SelectedIndex = 0;
+            }
+        }
+    }
+}
